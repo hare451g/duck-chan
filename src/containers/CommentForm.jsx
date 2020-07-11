6593295d44f682c2208116ed742aa11df85f35e5
hendra5 <hendra@stockbit.com>
2020-07-11 11:47:07 +0700

    implement useForm at comment and new thread

--- src/containers/CommentForm.jsx
+++ src/containers/CommentForm.jsx
@@ -4,18 +4,24 @@ import Flex from '../components/Flex';
 import Textarea from '../components/Textarea';
 import Text from '../components/Text';
 import Button from '../components/Button';
+import useForm from '../hooks/useForm';
 
 function CommentForm({
   threadName = 'untitled',
   onCancel = () => {},
   onPost = () => {},
 }) {
+  const [comment, setComment] = useForm('');
   return (
     <Flex direction="column" margin="16px 0 0 48px">
       <Text fontSize="14px" fontWeight="bold" margin="8px 0px">
         Reply to: {threadName}
       </Text>
-      <Textarea placeholder="Write comments . . . " />
+      <Textarea
+        placeholder="Write comments . . . "
+        value={comment}
+        onChange={setComment}
+      />
       <Flex justify="flex-end" align="center" margin="8px 0px">
         <Button variant="naked" color="muted" onClick={onCancel}>
           cancel
--- src/containers/NewThreadForm.jsx
+++ src/containers/NewThreadForm.jsx
@@ -5,8 +5,13 @@ import Text from '../components/Text';
 import TextInput from '../components/TextInput';
 import Textarea from '../components/Textarea';
 import Button from '../components/Button';
+import useForm from '../hooks/useForm';
 
-function NewThreadForm() {
+function NewThreadForm({ onPost = () => {}, onCancel = () => {} }) {
+  const [title, setTitle] = useForm('');
+  const [name, setName] = useForm('Anon');
+  const [content, setContent] = useForm('');
+
   return (
     <Flex direction="column">
       <Text fontSize="14px" fontWeight="bold">
@@ -17,27 +22,31 @@ function NewThreadForm() {
         <Text fontSize="14px" fontWeight="bold" margin="8px 0px">
           Title
         </Text>
-        <TextInput type="text" />
+        <TextInput type="text" value={title} onChange={setTitle} />
       </Flex>
       <Flex direction="column" margin="16px 0">
         <Text fontSize="14px" fontWeight="bold" margin="8px 0px">
           Post as
         </Text>
-        <TextInput type="text" />
+        <TextInput type="text" value={name} onChange={setName} />
       </Flex>
 
       <Flex direction="column" margin="16px 0">
         <Text fontSize="14px" fontWeight="bold" margin="8px 0px">
           Content
         </Text>
-        <Textarea placeholder="Write your thread" />
+        <Textarea
+          placeholder="Write your thread"
+          value={content}
+          onChange={setContent}
+        />
       </Flex>
 
       <Flex justify="flex-end" align="center" margin="8px 0px">
-        <Button variant="naked" color="muted">
+        <Button variant="naked" color="muted" onCancel={onCancel}>
           cancel
         </Button>
-        <Button variant="naked" color="primary">
+        <Button variant="naked" color="primary" onClick={onPost}>
           post
         </Button>
       </Flex>
