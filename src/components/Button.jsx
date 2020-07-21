586e9444a5f9bbefe9c7fd50cd2100ba81e27176
hendra5 <hendra@stockbit.com>
2020-07-21 12:11:39 +0700

    fix props condition for color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,7 @@ import './Button.css';
 
 function Button({ children, variant = '', color = '', ...rest }) {
   const variantClass = variant ? `btn-${variant}` : '';
-
-  const colorClass = variant ? `btn-${color}` : '';
+  const colorClass = color ? `btn-${color}` : '';
 
   return (
     <button className={`btn ${variantClass} ${colorClass}`} {...rest}>
